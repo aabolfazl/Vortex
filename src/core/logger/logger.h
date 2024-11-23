0bf58e84780cb7de360c93f67c1b87b5de4c4547
aabolfazl <aabolfazlit@gmail.com>
2024-11-24 00:02:12 +0300

    [refactor] Update logger strategy

--- src/core/logger/logger.h
+++ src/core/logger/logger.h
@@ -1,21 +1,21 @@
 /*
-  * Copyright © 2024 Vortex project
-  *
-  * This is the source code of the Vortex project.
-  * It is licensed under the MIT License; you should have received a copy
-  * of the license in this archive (see LICENSE).
-  *
-  * Author: Abolfazl Abbasi
-  *
-  */
+ * Copyright © 2024 Vortex project
+ *
+ * This is the source code of the Vortex project.
+ * It is licensed under the MIT License; you should have received a copy
+ * of the license in this archive (see LICENSE).
+ *
+ * Author: Abolfazl Abbasi
+ *
+ */
 
 #ifndef LOGGER_H
 #define LOGGER_H
 #pragma once
 
-#include <fmt/format.h>
-#include <fmt/chrono.h>
 #include <chrono>
+#include <fmt/chrono.h>
+#include <fmt/format.h>
 
 namespace vortex::core {
 class logger {
@@ -34,7 +34,7 @@ public:
 
 private:
     template <typename... Args>
-    static void log(const log_level level, const char* format, Args&&... args) {
+    static void log(const log_level level, const char *format, Args &&...args) {
         const auto now = std::chrono::system_clock::now();
         const auto time = std::chrono::system_clock::to_time_t(now);
 
@@ -44,16 +44,28 @@ private:
         fmt::format_to(std::back_inserter(buffer), format, std::forward<Args>(args)...);
         fmt::format_to(std::back_inserter(buffer), "\n");
 
-        fmt::print("{}", fmt::to_string(buffer));
+        std::string color_code;
+        if (level == log_level::info) {
+            color_code = "\033[34m";
+        } else if (level == log_level::error) {
+            color_code = "\033[31m";
+        } else {
+            color_code = "\033[0m";
+        }
+
+        fmt::print("{}{}{}", color_code, fmt::to_string(buffer), "\033[0m");
     }
 
-    static const char* log_level_to_string(const log_level level) {
-        switch (level){
-        case log_level::info: return "I";
-        case log_level::error: return "D";
-        default: return "UNKNOWN";
+    static const char *log_level_to_string(const log_level level) {
+        switch (level) {
+        case log_level::info:
+            return "I";
+        case log_level::error:
+            return "D";
+        default:
+            return "UNKNOWN";
         }
     }
 };
-}
-#endif //LOGGER_H
+} // namespace vortex::core
+#endif // LOGGER_H
