73b8c6f8d7cdff9bc9087550ba19b4ec222eff27
aabolfazl <aabolfazlit@gmail.com>
2024-11-24 11:40:20 +0300

    [chore] Add copyright notices to config and parser files

--- src/core/config/yaml_parser.cpp
+++ src/core/config/yaml_parser.cpp
@@ -1,3 +1,14 @@
+/*
+ * Copyright © 2024 Vortex project
+ *
+ * This is the source code of the Vortex project.
+ * It is licensed under the MIT License; you should have received a copy
+ * of the license in this archive (see LICENSE).
+ *
+ * Author: Abolfazl Abbasi
+ *
+ */
+
 #include "yaml_parser.h"
 #include <stdexcept>
 
