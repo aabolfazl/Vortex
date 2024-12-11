2f3c457789215c0d3fe82c701e5803678f7d8f3a
aabolfazl <aabolfazlit@gmail.com>
2024-12-11 21:12:56 +0000

    [feat] add logger macros

--- src/core/logger/logger.h
+++ src/core/logger/logger.h
@@ -9,8 +9,6 @@
  *
  */
 
-#ifndef LOGGER_H
-#define LOGGER_H
 #pragma once
 
 #include <chrono>
@@ -19,6 +17,16 @@
 #include <iostream>
 #include <unistd.h>
 
+constexpr std::string_view extract_filename(std::string_view path) {
+    size_t pos = path.find_last_of("/\\");
+    return (pos == std::string_view::npos) ? path : path.substr(pos + 1);
+}
+
+#define LOG_I(fmt, ...) vortex::core::logger::info(extract_filename(__FILE__).data(), fmt, ##__VA_ARGS__)
+#define LOG_E(fmt, ...) vortex::core::logger::error(extract_filename(__FILE__).data(), fmt, ##__VA_ARGS__)
+#define LOG_W(fmt, ...) vortex::core::logger::warn(extract_filename(__FILE__).data(), fmt, ##__VA_ARGS__)
+#define LOG_D(fmt, ...) vortex::core::logger::debug(extract_filename(__FILE__).data(), fmt, ##__VA_ARGS__)
+
 namespace vortex::core {
 class logger {
 public:
@@ -26,40 +34,63 @@ public:
 
     template <typename... Args>
     static void info(const char* format, Args&&... args) {
-        log(log_level::info, format, std::forward<Args>(args)...);
+        log(log_level::info,nullptr, format, std::forward<Args>(args)...);
+    }
+    template <typename... Args>
+    static void info(const char* filename, const char* format, Args&&... args) {
+        log(log_level::info, filename, format, std::forward<Args>(args)...);
     }
 
     template <typename... Args>
     static void error(const char* format, Args&&... args) {
-        log(log_level::error, format, std::forward<Args>(args)...);
+        log(log_level::error, nullptr, format, std::forward<Args>(args)...);
+    }
+    template <typename... Args>
+    static void error(const char* filename, const char* format, Args&&... args) {
+        log(log_level::error, filename, format, std::forward<Args>(args)...);
     }
 
     template <typename... Args>
-    static void warning(const char* format, Args&&... args) {
-        log(log_level::warning, format, std::forward<Args>(args)...);
+    static void warn(const char* format, Args&&... args) {
+        log(log_level::warning, nullptr, format, std::forward<Args>(args)...);
     }
+    template <typename... Args>
+    static void warn(const char* filename, const char* format, Args&&... args) {
+        log(log_level::warning, filename, format, std::forward<Args>(args)...);
+    }
+
 
     template <typename... Args>
     static void debug(const char* format, Args&&... args) {
         log(log_level::debug, format, std::forward<Args>(args)...);
     }
+    template <typename... Args>
+    static void debug(const char* filename, const char* format, Args&&... args) {
+        log(log_level::debug, filename, format, std::forward<Args>(args)...);
+    }
 
 private:
     template <typename... Args>
-    static void log(const log_level level, const char* format, Args&&... args) {
+    static void log(const log_level level, const char* filename, const char* format, Args&&... args) {
         const auto now = std::chrono::system_clock::now();
         const auto time = std::chrono::system_clock::to_time_t(now);
 
+
         fmt::memory_buffer buffer;
-        fmt::format_to(std::back_inserter(buffer), "{:%Y-%m-%d %H:%M:%S} {} {} ", *std::localtime(&time), getpid(),
-                       log_level_to_string(level));
+
+        if (filename) {
+            fmt::format_to(std::back_inserter(buffer), "{:%Y-%m-%d %H:%M:%S} {} {} {} : ", *std::localtime(&time),
+                           getpid(), log_level_to_string(level), filename);
+        } else {
+            fmt::format_to(std::back_inserter(buffer), "{:%Y-%m-%d %H:%M:%S} {} {} : ", *std::localtime(&time),
+                           getpid(), log_level_to_string(level));
+        }
 
         fmt::format_to(std::back_inserter(buffer), format, std::forward<Args>(args)...);
         fmt::format_to(std::back_inserter(buffer), "\n");
 
         std::string color_code;
         switch (level) {
-
         case log_level::error:
             color_code = "\033[31m";
             break;
@@ -93,4 +124,3 @@ private:
     }
 };
 } // namespace vortex::core
-#endif // LOGGER_H
